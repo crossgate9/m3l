3a4d622bc815c28ee77f3c6cde8c288c6c5bc68b
Allen Koo <crossgate9@gmail.com>
2017-04-05 18:50:21 +0800

    [refactor] alpha, beta parameter search for MCDE

--- mcde_kol_3_alpha_beta.m
+++ mcde_kol_3_alpha_beta.m
@@ -19,38 +19,42 @@ knn = 1;
 [dimension, sigmak] = meshgrid(dimension_, sigmak_);
 n = numel(sigmak_);
 
+alpha = [1 0.5 0.1];
+beta = [1 0.5 0.1];
+
 fprintf('ratio 0.1 ... \n');
 for i=1:n,
+    options.fscore_beta = 2;
     options.knn = knn;
     options.dimension = 1;
-    
-    options.alpha = 1;
-    options.beta = 1;
     options.sigmak = sigmak_(i);
-    R = exp_acmmm(X, L, options);
-    fprintf('dimension=%d, sigmak=%d, alpha=%d, beta=%d, f1=%d\n', ...
-        dimension(i), sigmak_(i), options.alpha, options.beta, R.f);
-    filename = sprintf('W_%d_%d_%d.mat', options.alpha, options.beta, options.sigmak);
-    W = R.W;
-    save(filename, 'W');
-    
-    options.alpha = 0.5;
-    options.beta = 0.5;
-    options.sigmak = sigmak_(i);
-    R = exp_acmmm(X, L, options);
-    fprintf('dimension=%d, sigmak=%d, alpha=%d, beta=%d, f1=%d\n', ...
-        dimension(i), sigmak_(i), options.alpha, options.beta, R.f);
-    filename = sprintf('W_%d_%d_%d.mat', options.alpha, options.beta, options.sigmak);
-    W = R.W;
-    save(filename, 'W');
-    
-    options.alpha = 0.1;
-    options.beta = 0.1;
-    options.sigmak = sigmak_(i);
-    R = exp_acmmm(X, L, options);
-    fprintf('dimension=%d, sigmak=%d, alpha=%d, beta=%d, f1=%d\n', ...
-        dimension(i), sigmak_(i), options.alpha, options.beta, R.f);
-    filename = sprintf('W_%d_%d_%d.mat', options.alpha, options.beta, options.sigmak);
-    W = R.W;
-    save(filename, 'W');
+
+    for j=1:3,
+        options.alpha = alpha(j);
+        options.beta = beta(j);
+        filename = sprintf('%s/W_%d_%d_%d.mat', drfun, options.alpha, options.beta, options.sigmak);
+        if exist(filename),
+            use_cache = true;
+        else
+            use_cache = false;
+        end
+
+        if use_cache,
+            load(filename);
+            options.W = W;
+        else
+            if isfield(options, 'W'),
+                options = rmfield(options, 'W');
+            end
+        end
+
+        R = exp_acmmm(X, L, options);
+        fprintf('dimension=%d, sigmak=%d, alpha=%d, beta=%d, f=%d\n', ...
+            dimension(i), sigmak_(i), options.alpha, options.beta, R.f);
+
+        if ~ use_cache,
+            W = R.W;
+            save(filename, 'W');
+        end
+    end
 end
