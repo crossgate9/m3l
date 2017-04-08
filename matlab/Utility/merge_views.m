a439f9710ff336ca1493c2990ab1b7451b833dd9
Allen Koo <crossgate9@gmail.com>
2017-04-08 20:03:19 +0800

    [bug fix]

--- matlab/Utility/merge_views.m
+++ matlab/Utility/merge_views.m
@@ -1,7 +1,15 @@
 function D = merge_views(X, L)
+    % X V*1 cell, for each cell, Dv*n
+    % L: V*n
 
 V = length(X);
 n = size(L, 2);
+
+if size(X{1}, 2) ~= n,
+    for i=1:V,
+        X{i} = X{i}';
+    end
+end
 
 % merge data into big matrix
 d = 0;
@@ -13,4 +21,5 @@ j = 0;
 D = zeros(n, d);
 for i=1:V,
     D(:, j+1:j+size(X{i}, 1)) = X{i}';
+    j = j + size(X{i}, 1);
 end
